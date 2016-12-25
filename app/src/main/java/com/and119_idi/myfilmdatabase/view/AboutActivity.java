6ea8647ba93e16dd55fbee9e77d4225daf840b86
AlbertLopezAlcacer <alcacer.la.1001@gmail.com>
2016-12-25 23:13:36 +0100

    about small fixes

--- app/src/main/java/com/and119_idi/myfilmdatabase/view/AboutActivity.java
+++ app/src/main/java/com/and119_idi/myfilmdatabase/view/AboutActivity.java
@@ -1,6 +1,9 @@
 package com.and119_idi.myfilmdatabase.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.and119_idi.myfilmdatabase.R;
 
@@ -9,10 +12,28 @@ import com.and119_idi.myfilmdatabase.R;
  */
 public class AboutActivity extends DialogActivity {
 
+    private ImageButton mMailButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_layout);
+        initCoolStuff();
+    }
+
+    private void initCoolStuff() {
+        mMailButton = (ImageButton) findViewById(R.id.contact);
+        mMailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("plain/text");
+                intent.putExtra(Intent.EXTRA_SUBJECT, "2kg of your best marijuana please");
+                intent.putExtra(Intent.EXTRA_EMAIL,
+                        new String[] { getApplicationContext().getString(R.string.mail_albert),
+                                       getApplicationContext().getString(R.string.mail_carlos) });
+                startActivity(Intent.createChooser(intent, ""));
+            }
+        });
     }
 
     @Override
