35e7b1cc1d2c2df9afa5f0d95bcff6ea2b2d8304
Carleslc <lazaro.costa.carles@gmail.com>
2016-12-26 04:46:34 +0100

    Minor fixes

--- app/src/main/java/com/and119_idi/myfilmdatabase/view/CheckableDialogActivity.java
+++ app/src/main/java/com/and119_idi/myfilmdatabase/view/CheckableDialogActivity.java
@@ -30,6 +30,7 @@ public abstract class CheckableDialogActivity extends DialogActivity {
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
+                        unsavedData = false;
                         fi();
                     }
                 });
