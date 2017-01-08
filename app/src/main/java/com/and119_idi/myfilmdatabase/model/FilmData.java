4c37bdb471f9c7512550d5ed0137fdcca8dfcc4f
Carleslc <lazaro.costa.carles@gmail.com>
2017-01-08 17:57:38 +0100

    onCreate NullPointerException fix + Description details no scroll

--- app/src/main/java/com/and119_idi/myfilmdatabase/model/FilmData.java
+++ app/src/main/java/com/and119_idi/myfilmdatabase/model/FilmData.java
@@ -1,6 +1,5 @@
 package com.and119_idi.myfilmdatabase.model;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.SQLException;
@@ -12,6 +11,8 @@ import android.util.Log;
 import java.io.Closeable;
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.and119_idi.myfilmdatabase.model.MySQLiteHelper.filmToValues;
 
 /**
  * Created by pr_idi on 10/11/16.
@@ -36,7 +37,7 @@ public class FilmData implements Closeable {
     };
 
     public FilmData(Context context) {
-        mDatabaseHelper = new MySQLiteHelper(context, this);
+        mDatabaseHelper = new MySQLiteHelper(context);
     }
 
     public void open() throws SQLException {
@@ -48,50 +49,11 @@ public class FilmData implements Closeable {
         mDatabaseHelper.close();
     }
 
-    public void addDefaultFilms() {
-        addFilm(new Film("The Shawshank Redemption", "Frank Darabont", "Canada", 1994, "Tim Robbins",
-                "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.", 9));
-        addFilm(new Film("The Godfather", "Francis Ford Coppola", "United States", 1972, "Marlon Brando",
-                "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.", 9));
-        addFilm(new Film("Apocalypse Now", "Francis Ford Coppola", "United States", 1979, "Marlon Brando",
-                "During the Vietnam War, Captain Willard is sent on a dangerous mission into Cambodia to assassinate a renegade colonel who has set himself up as a god among a local tribe.", 8));
-        addFilm(new Film("Star Wars", "George Lucas", "United States", 1977, "Marlon Brando",
-                "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a wookiee and two droids to save the galaxy from the Empire's world-destroying battle-station, while also attempting to rescue Princess Leia from the evil Darth Vader.", 9));
-        addFilm(new Film("Pan's Labyrinth", "Guillermo del Toro", "Spain", 2006, "Ariadna Gil",
-                "In the falangist Spain of 1944, the bookish young stepdaughter of a sadistic army officer escapes into an eerie but captivating fantasy world.", 8));
-        addFilm(new Film("The Matrix", "The Wachowski Brothers", "United States", 1999, "Keanu Reeves",
-                "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.", 9));
-        addFilm(new Film("Forrest Gump", "Robert Zemeckis", "United States", 1994, "Tom Hanks",
-                "Forrest Gump, while not intelligent, has accidentally been present at many historic moments, but his true love, Jenny Curran, eludes him.", 9));
-        addFilm(new Film("The Lord of the Rings: The Return of the King", "Peter Jackson", "New Zealand", 2003, "Elijah Wood",
-                "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.", 9));
-        addFilm(new Film("Pulp Fiction", "Quentin Tarantino", "United States", 1994, "John Travolta",
-                "The lives of two mob hit men, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.", 9));
-        addFilm(new Film("Sen to Chihiro no kamikakushi", "Hayao Miyazaki", "Japan", 2001, "Daveigh Chase",
-                "During her family's move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches, and spirits, and where humans are changed into beasts.", 9));
-        addFilm(new Film("The Dark Knight", "Christopher Nolan", "United Kingdom", 2008, "Christian Bale",
-                "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.", 9));
-    }
-
     public void deleteFilm(Film film) {
         long id = film.getId();
         int rows = mDatabase.delete(MySQLiteHelper.TABLE_FILMS, MySQLiteHelper.COLUMN_ID
                 + " = " + id, null);
         Log.d(TAG, "Film deleted with id: " + id + ((rows > 0) ? " (SUCCESS)" : "(FAIL)"));
-    }
-
-    private ContentValues filmToValues(@NonNull Film film) {
-        ContentValues values = new ContentValues();
-
-        values.put(MySQLiteHelper.COLUMN_TITLE, film.getTitle());
-        values.put(MySQLiteHelper.COLUMN_DIRECTOR, film.getDirector());
-        values.put(MySQLiteHelper.COLUMN_COUNTRY, film.getCountry());
-        values.put(MySQLiteHelper.COLUMN_YEAR_RELEASE, film.getYear());
-        values.put(MySQLiteHelper.COLUMN_PROTAGONIST, film.getProtagonist());
-        values.put(MySQLiteHelper.COLUMN_CRITICS_RATE, film.getCriticsRate());
-        values.put(MySQLiteHelper.COLUMN_DESCRIPTION, film.getDescription());
-
-        return values;
     }
 
     private boolean exists(@NonNull Film film) {
@@ -102,13 +64,7 @@ public class FilmData implements Closeable {
 
     public void addFilm(@NonNull Film film) {
         if (exists(film)) updateFilm(film);
-        else insertNewFilm(film);
-    }
-
-    private void insertNewFilm(@NonNull Film film) {
-        mDatabase.insert(MySQLiteHelper.TABLE_FILMS, null, filmToValues(film));
-        Log.d(TAG, "insertNewFilm " + film.getTitle() + " ID-AUTOINCREMENT"
-                + " with description " + film.getDescription());
+        else MySQLiteHelper.insertNewFilm(mDatabase, film);
     }
 
     private void updateFilm(@NonNull Film film) {
--- app/src/main/java/com/and119_idi/myfilmdatabase/model/MySQLiteHelper.java
+++ app/src/main/java/com/and119_idi/myfilmdatabase/model/MySQLiteHelper.java
@@ -1,8 +1,10 @@
 package com.and119_idi.myfilmdatabase.model;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.NonNull;
 import android.util.Log;
 
 /**
@@ -20,6 +22,7 @@ class MySQLiteHelper extends SQLiteOpenHelper {
     static final String COLUMN_CRITICS_RATE = "critics_rate";
     static final String COLUMN_DESCRIPTION = "description";
 
+    private static final String TAG = MySQLiteHelper.class.getSimpleName();
     private static final String DATABASE_NAME = "films.db";
     private static final int DATABASE_VERSION = 2;
     // Database creation sql statement
@@ -33,17 +36,60 @@ class MySQLiteHelper extends SQLiteOpenHelper {
             + COLUMN_CRITICS_RATE + " integer, "
             + COLUMN_DESCRIPTION + " text"
             + ");";
-    private FilmData mFilmData;
 
-    MySQLiteHelper(Context context, FilmData filmData) {
+    MySQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        mFilmData = filmData;
+    }
+
+    static void insertNewFilm(SQLiteDatabase database, @NonNull Film film) {
+        database.insert(MySQLiteHelper.TABLE_FILMS, null, filmToValues(film));
+        Log.d(TAG, "insertNewFilm " + film.getTitle() + " ID-AUTOINCREMENT"
+                + " with description " + film.getDescription());
+    }
+
+    static ContentValues filmToValues(@NonNull Film film) {
+        ContentValues values = new ContentValues();
+
+        values.put(MySQLiteHelper.COLUMN_TITLE, film.getTitle());
+        values.put(MySQLiteHelper.COLUMN_DIRECTOR, film.getDirector());
+        values.put(MySQLiteHelper.COLUMN_COUNTRY, film.getCountry());
+        values.put(MySQLiteHelper.COLUMN_YEAR_RELEASE, film.getYear());
+        values.put(MySQLiteHelper.COLUMN_PROTAGONIST, film.getProtagonist());
+        values.put(MySQLiteHelper.COLUMN_CRITICS_RATE, film.getCriticsRate());
+        values.put(MySQLiteHelper.COLUMN_DESCRIPTION, film.getDescription());
+
+        return values;
     }
 
     @Override
     public void onCreate(SQLiteDatabase database) {
         database.execSQL(DATABASE_CREATE);
-        mFilmData.addDefaultFilms();
+        addDefaultFilms(database);
+    }
+
+    private void addDefaultFilms(SQLiteDatabase database) {
+        insertNewFilm(database, new Film("The Shawshank Redemption", "Frank Darabont", "Canada", 1994, "Tim Robbins",
+                "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.", 9));
+        insertNewFilm(database, new Film("The Godfather", "Francis Ford Coppola", "United States", 1972, "Marlon Brando",
+                "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.", 9));
+        insertNewFilm(database, new Film("Apocalypse Now", "Francis Ford Coppola", "United States", 1979, "Marlon Brando",
+                "During the Vietnam War, Captain Willard is sent on a dangerous mission into Cambodia to assassinate a renegade colonel who has set himself up as a god among a local tribe.", 8));
+        insertNewFilm(database, new Film("Star Wars", "George Lucas", "United States", 1977, "Mark Hamill",
+                "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a wookiee and two droids to save the galaxy from the Empire's world-destroying battle-station, while also attempting to rescue Princess Leia from the evil Darth Vader.", 9));
+        insertNewFilm(database, new Film("Pan's Labyrinth", "Guillermo del Toro", "Spain", 2006, "Ariadna Gil",
+                "In the falangist Spain of 1944, the bookish young stepdaughter of a sadistic army officer escapes into an eerie but captivating fantasy world.", 8));
+        insertNewFilm(database, new Film("The Matrix", "The Wachowski Brothers", "United States", 1999, "Keanu Reeves",
+                "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.", 9));
+        insertNewFilm(database, new Film("Forrest Gump", "Robert Zemeckis", "United States", 1994, "Tom Hanks",
+                "Forrest Gump, while not intelligent, has accidentally been present at many historic moments, but his true love, Jenny Curran, eludes him.", 9));
+        insertNewFilm(database, new Film("The Lord of the Rings: The Return of the King", "Peter Jackson", "New Zealand", 2003, "Elijah Wood",
+                "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.", 9));
+        insertNewFilm(database, new Film("Pulp Fiction", "Quentin Tarantino", "United States", 1994, "John Travolta",
+                "The lives of two mob hit men, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption.", 9));
+        insertNewFilm(database, new Film("Sen to Chihiro no kamikakushi", "Hayao Miyazaki", "Japan", 2001, "Daveigh Chase",
+                "During her family's move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches, and spirits, and where humans are changed into beasts.", 9));
+        insertNewFilm(database, new Film("The Dark Knight", "Christopher Nolan", "United Kingdom", 2008, "Christian Bale",
+                "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.", 9));
     }
 
     @Override
